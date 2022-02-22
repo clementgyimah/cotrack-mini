80a23c94b9c018a4e568185fb01d1b9c09c6629c
clementgyimah <clementgyimah2@gmail.com>
2022-02-22 11:51:51 +0000

    feat: 🎸 asterick icon component added

---
+++ src/components/Icon.js
@@ -0,0 +1,9 @@
+import React from "react";
+import '../assets/css/Icon.css'
+import { FaAsterisk } from "react-icons/fa";
+
+const FaAsteriskIcon = (props) => {
+  return <FaAsterisk className="asterick-icon" style={props.style}/>;
+};
+
+export { FaAsteriskIcon }
