a94cc45a0ef9fe0da506deca0b283cd5dcb0455c
clementgyimah <clementgyimah2@gmail.com>
2022-02-23 13:01:35 +0000

    refactor: 💡 function for adding attendee to system seperated

--- src/screens/attendance/components/NewAttendee.js
+++ src/screens/attendance/components/NewAttendee.js
@@ -1,10 +1,14 @@
 // Calling all necessary packages and libraries
 import React, { useState } from "react";
 import "../../../assets/css/NewAttendee.css";
-import { NewAttendeeFormInputStyle, NewAttendeeFormSelectStyle, AsterickIcon } from "../../../assets/styles";
+import {
+  NewAttendeeFormInputStyle,
+  NewAttendeeFormSelectStyle,
+  AsterickIcon,
+} from "../../../assets/styles";
 import { BsToggleOff, BsToggleOn } from "react-icons/bs";
 import { TextInput, SelectInput, FaAsteriskIcon } from "../../../components";
-const { ipcRenderer } = window.require("electron");
+import { addNewAttendee } from "../functions";
 
 export default function NewAttendee(props) {
   // declaration of state variables
@@ -17,135 +21,23 @@ export default function NewAttendee(props) {
   const [temperature, setTemperature] = useState("");
   const [attendeeAlreadyExist, setAttendeeAlreadyExist] = useState(false);
   const [invalidAdd, setInvalidAdd] = useState(false);
-  const [activateTemp, setActivateTemp] = useState(true);
-  const [activateEmail, setActivateEmail] = useState(true);
-  const [activateContact, setActivateContact] = useState(true);
-  const [activateLocation, setActivateLocation] = useState(true);
+  const [activateTemp, setActivateTemp] = useState(false);
   const [invalidEmail, setInvalidEmail] = useState(false);
 
-  const onSubmit = () => {
-    // variables to hold attendee details
-    setFirstName(firstName.split(" ").join(""));
-    setLastName(lastName.split(" ").join(""));
-    setContactNumber(contactNumber.split(" ").join(""));
-    setEmailAddress(emailAddress.split(" ").join(""));
-    setTemperature(temperature.split(" ").join(""));
-    setGender(gender.split(" ").join(""));
-    console.log(gender);
-    /**
-     * logic that uses "Exclusive OR" to make sure that each toggle button
-     * correlate with it's input before form submission
-     */
-    if (firstName.length === 0 || lastName.length === 0 || gender.length === 0)
-      return setInvalidAdd(true);
-    else if (
-      location.length === 0 ||
-      contactNumber.length === 0 ||
-      emailAddress.length === 0 ||
-      temperature.length === 0
-    ) {
-      if (
-        (location.length === 0 && !activateLocation) ||
-        (!(location.length === 0) && activateLocation)
-      ) {
-        if (
-          (contactNumber.length === 0 && !activateContact) ||
-          (!(contactNumber.length === 0) && activateContact)
-        ) {
-          if (
-            (emailAddress.length === 0 && !activateEmail) ||
-            (!(emailAddress.length === 0) && activateEmail)
-          ) {
-            if (
-              (temperature.length === 0 && !activateTemp) ||
-              (!(temperature.length === 0) && activateTemp)
-            ) {
-              if (!activateEmail) {
-                const attendeeDetails = {
-                  firstName,
-                  lastName,
-                  gender,
-                  location,
-                  contactNumber,
-                  emailAddress,
-                  temperature,
-                };
-                ipcRenderer.send("new-attendee", attendeeDetails);
-                ipcRenderer.on("new-attendee-reply", (event, arg) => {
-                  setAttendeeAlreadyExist(arg);
-                });
-                /**
-                 * reset each variable as empty string.
-                 * This will clear all input values
-                 */
-                setFirstName("");
-                setLastName("");
-                setLocation("");
-                setContactNumber("");
-                setEmailAddress("");
-                setTemperature("");
-              } else {
-                /**
-                 * condition to check if a valid email is entered
-                 * in the email input. Done by checking '@' sign in the input
-                 */
-                if (emailAddress.indexOf("@") < 0) setInvalidAdd(true);
-                else {
-                  if (emailAddress.length > 25) setInvalidEmail(true);
-                  else {
-                    const attendeeDetails = {
-                      firstName,
-                      lastName,
-                      gender,
-                      location,
-                      contactNumber,
-                      emailAddress,
-                      temperature,
-                    };
-                    ipcRenderer.send("new-attendee", attendeeDetails);
-                    ipcRenderer.on("new-attendee-reply", (event, arg) => {
-                      setAttendeeAlreadyExist(arg);
-                    });
-                    setFirstName("");
-                    setLastName("");
-                    setLocation("");
-                    setContactNumber("");
-                    setEmailAddress("");
-                    setTemperature("");
-                  }
-                }
-              }
-            } else setInvalidAdd(true);
-          } else setInvalidAdd(true);
-        } else setInvalidAdd(true);
-      } else setInvalidAdd(true);
-    } else if (emailAddress.indexOf("@") < 0) {
-      setInvalidAdd(true);
-    } else {
-      if (emailAddress.length > 25) setInvalidEmail(true);
-      else {
-        const attendeeDetails = {
-          firstName,
-          lastName,
-          gender,
-          location,
-          contactNumber,
-          emailAddress,
-          temperature,
-        };
-        ipcRenderer.send("new-attendee", attendeeDetails);
-        ipcRenderer.on("new-attendee-reply", (event, arg) => {
-          setAttendeeAlreadyExist(arg);
-        });
-        setFirstName("");
-        setLastName("");
-        setLocation("");
-        setContactNumber("");
-        setEmailAddress("");
-        setTemperature("");
-      }
-    }
+  const resetAllInputs = () => {
+    setFirstName("");
+    setLastName("");
+    setLocation("");
+    setContactNumber("");
+    setEmailAddress("");
+    setTemperature("");
   };
+
+  const resetAllErrors = () => {
+    setAttendeeAlreadyExist(false);
+    setInvalidAdd(false);
+    setInvalidEmail(false);
+  }
 
   // expressions to set inputs to their respective state variables
   const firstNameFunc = (e) => {
@@ -204,27 +96,6 @@ export default function NewAttendee(props) {
     setInvalidEmail(false);
   };
 
-  const activateEmailFunc = () => {
-    setActivateEmail(!activateEmail);
-    setEmailAddress("");
-    setInvalidAdd(false);
-    setInvalidEmail(false);
-  };
-
-  const activateContactFunc = () => {
-    setActivateContact(!activateContact);
-    setContactNumber("");
-    setInvalidAdd(false);
-    setInvalidEmail(false);
-  };
-
-  const activateLocationFunc = () => {
-    setActivateLocation(!activateLocation);
-    setLocation("");
-    setInvalidAdd(false);
-    setInvalidEmail(false);
-  };
-
   return (
     <div className="new-attendee-container">
       <div className="new-attendee-holder">
@@ -270,167 +141,61 @@ export default function NewAttendee(props) {
             selectStyle={NewAttendeeFormSelectStyle}
             options={[
               {
-                displayName: 'Male',
-                value: 'Male'
+                displayName: "Male",
+                value: "Male",
               },
               {
-                displayName: 'Female',
-                value: 'Female'
-              }
+                displayName: "Female",
+                value: "Female",
+              },
             ]}
           />
           <FaAsteriskIcon style={AsterickIcon} />
         </div>
         <div className="section-div">
-          <label
-            htmlFor="location-label"
-            className={
-              activateLocation
-                ? "attendee-details-text"
-                : "attendee-details-text-disabled"
-            }
-          >
+          <label htmlFor="location-label" className="attendee-details-text">
             Location:{" "}
           </label>
-          {activateLocation ? (
-            <TextInput
-              type="text"
-              id="location-label"
-              placeholder="Aputuogya"
-              value={location}
-              onChange={locationFunc}
-              inputStyle={NewAttendeeFormInputStyle}
-            />
-          ) : (
-            <TextInput
-              disabled={true}
-              type="text"
-              id="location-label"
-              placeholder="Aputuogya"
-              value={location}
-              onChange={locationFunc}
-              inputStyle={NewAttendeeFormInputStyle}
-            />
-          )}
-
-          <span
-            className="activate-toggle-span"
-            onClick={() => activateLocationFunc()}
-          >
-            {activateLocation ? (
-              <BsToggleOn
-                className="activate-toggle-on"
-                size={30}
-                color="#387C44"
-              />
-            ) : (
-              <BsToggleOff
-                className="activate-toggle-off"
-                size={30}
-                color="#387C44"
-              />
-            )}
-          </span>
+          <TextInput
+            type="text"
+            id="location-label"
+            placeholder="Aputuogya"
+            value={location}
+            onChange={locationFunc}
+            inputStyle={NewAttendeeFormInputStyle}
+          />
         </div>
         <div className="section-div">
           <label
             htmlFor="contact-number-label"
-            className={
-              activateContact
-                ? "attendee-details-text"
-                : "attendee-details-text-disabled"
-            }
+            className="attendee-details-text"
           >
             Contact Number:{" "}
           </label>
-          {activateContact ? (
-            <TextInput
-              type="text"
-              id="contact-number-label"
-              placeholder="0559505063"
-              value={contactNumber}
-              onChange={contactNumberFunc}
-              inputStyle={NewAttendeeFormInputStyle}
-            />
-          ) : (
-            <TextInput
-              disabled={true}
-              type="text"
-              id="contact-number-label"
-              placeholder="0559505063"
-              value={contactNumber}
-              onChange={contactNumberFunc}
-              inputStyle={NewAttendeeFormInputStyle}
-            />
-          )}
-          <span
-            className="activate-toggle-span"
-            onClick={() => activateContactFunc()}
-          >
-            {activateContact ? (
-              <BsToggleOn
-                className="activate-toggle-on"
-                size={30}
-                color="#387C44"
-              />
-            ) : (
-              <BsToggleOff
-                className="activate-toggle-off"
-                size={30}
-                color="#387C44"
-              />
-            )}
-          </span>
+          <TextInput
+            type="text"
+            id="contact-number-label"
+            placeholder="0559505063"
+            value={contactNumber}
+            onChange={contactNumberFunc}
+            inputStyle={NewAttendeeFormInputStyle}
+          />
         </div>
         <div className="section-div">
           <label
             htmlFor="email-address-label"
-            className={
-              activateEmail
-                ? "attendee-details-text"
-                : "attendee-details-text-disabled"
-            }
+            className="attendee-details-text"
           >
             Email Address:{" "}
           </label>
-          {activateEmail ? (
-            <TextInput
-              type="text"
-              id="email-address-label"
-              placeholder="clementgyimah2@gmail.com"
-              value={emailAddress}
-              onChange={emailAddressFunc}
-              inputStyle={NewAttendeeFormInputStyle}
-            />
-          ) : (
-            <TextInput
-              disabled={true}
-              type="text"
-              id="email-address-label"
-              placeholder="clementgyimah2@gmail.com"
-              value={emailAddress}
-              onChange={emailAddressFunc}
-              inputStyle={NewAttendeeFormInputStyle}
-            />
-          )}
-          <span
-            className="activate-toggle-span"
-            onClick={() => activateEmailFunc()}
-          >
-            {activateEmail ? (
-              <BsToggleOn
-                className="activate-toggle-on"
-                size={30}
-                color="#387C44"
-              />
-            ) : (
-              <BsToggleOff
-                className="activate-toggle-off"
-                size={30}
-                color="#387C44"
-              />
-            )}
-          </span>
+          <TextInput
+            type="text"
+            id="email-address-label"
+            placeholder="clementgyimah2@gmail.com"
+            value={emailAddress}
+            onChange={emailAddressFunc}
+            inputStyle={NewAttendeeFormInputStyle}
+          />
         </div>
         <div className="section-div">
           <label
@@ -483,7 +248,31 @@ export default function NewAttendee(props) {
           </span>
         </div>
         <div className="temperature-button-div">
-          <div onClick={() => onSubmit()} className="temperature-button">
+          <div
+            onClick={() => addNewAttendee({
+              firstName,
+              lastName,
+              gender,
+              location,
+              contactNumber,
+              emailAddress,
+              temperature,
+              activateTemp,
+              setFirstName,
+              setLastName,
+              setGender,
+              setLocation,
+              setContactNumber,
+              setEmailAddress,
+              setTemperature,
+              setAttendeeAlreadyExist,
+              setInvalidAdd,
+              setInvalidEmail,
+              resetAllInputs,
+              resetAllErrors
+            })}
+            className="temperature-button"
+          >
             Save
           </div>
         </div>
