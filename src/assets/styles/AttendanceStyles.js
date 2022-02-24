54114de7159bd79fd924d9cc7138152d36ed6b6c
clementgyimah <clementgyimah2@gmail.com>
2022-02-24 11:25:16 +0000

    style: 💄 style for toggle icon

--- src/assets/styles/AttendanceStyles.js
+++ src/assets/styles/AttendanceStyles.js
@@ -6,8 +6,13 @@ const NewAttendeeFormSelectStyle = {
     // width: 400
 }
 
-const AsterickIcon = {
+const AsterickIconStyle = {
     fontSize: 10
 }
 
-export { NewAttendeeFormInputStyle, NewAttendeeFormSelectStyle, AsterickIcon }
+const toggleIconStyle = {
+    fontSize: 30,
+    color: '#387C44'
+}
+
+export { NewAttendeeFormInputStyle, NewAttendeeFormSelectStyle, AsterickIconStyle, toggleIconStyle }
