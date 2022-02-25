5923890529bfac33484af4188c0601737780fda4
clementgyimah <clementgyimah2@gmail.com>
2022-02-25 12:31:52 +0000

    feat: 🎸 styles for components added

--- src/assets/styles/AttendanceStyles.js
+++ src/assets/styles/AttendanceStyles.js
@@ -1,18 +1,49 @@
-const NewAttendeeFormInputStyle = {
-    // width: 400
-}
+const newAttendeeFormInputStyle = {
+    width: '40%',
+    height: 25,
+    border: 'solid 2px #387C44',
+    borderRadius: 8,
+    cursor: 'pointer',
+    fontSize: 15,
+    color: 'rgba(0, 0, 0, 0.7)'
+};
 
-const NewAttendeeFormSelectStyle = {
-    // width: 400
-}
+const newAttendeeFormSelectStyle = {
+    width: '40%',
+    height: 25,
+    border: 'solid 2px #387C44',
+    borderRadius: 8,
+    cursor: 'pointer',
+    fontSize: 15,
+    color: 'rgba(0, 0, 0, 0.7)'
+};
 
-const AsterickIconStyle = {
-    fontSize: 10
-}
+const asterickIconStyle = {
+    fontSize: 10,
+    margin: 10,
+    color: 'red'
+};
 
 const toggleIconStyle = {
-    fontSize: 30,
-    color: '#387C44'
-}
+  fontSize: 30,
+  color: "#387C44",
+};
 
-export { NewAttendeeFormInputStyle, NewAttendeeFormSelectStyle, AsterickIconStyle, toggleIconStyle }
+const oldAttendeeSearchInputStyle = {
+  border: "solid 2px #387C44",
+  width: "50%",
+  height: 30,
+  borderRadius: 20,
+  fontSize: 20,
+  display: "flex",
+  color: "rgba(0, 0, 0, 0.7)",
+  paddingLeft: 10,
+};
+
+export {
+  newAttendeeFormInputStyle,
+  newAttendeeFormSelectStyle,
+  asterickIconStyle,
+  toggleIconStyle,
+  oldAttendeeSearchInputStyle,
+};
