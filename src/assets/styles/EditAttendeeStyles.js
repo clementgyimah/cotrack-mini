de33941000c960d916abc3a28989fd611fb1f981
clementgyimah <clementgyimah2@gmail.com>
2023-01-27 19:31:39 +0000

    feat(editattendee styles): edit attendee styles added

---
+++ src/assets/styles/EditAttendeeStyles.js
@@ -0,0 +1,12 @@
+const editAttendeeSearchInputStyle = {
+  border: "solid 2px #387C44",
+  width: "50%",
+  height: 30,
+  borderRadius: 20,
+  fontSize: 20,
+  display: "flex",
+  color: "rgba(0, 0, 0, 0.7)",
+  paddingLeft: 10,
+};
+
+export { editAttendeeSearchInputStyle };
